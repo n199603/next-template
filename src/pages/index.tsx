02d03addcbbab44933735b63541ae2301f146048
Yuto Nakamura <yutonakamura0320@gmail.com>
2022-12-27 18:46:29 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Box, chakra } from '@chakra-ui/react';
+
+const Home = () => {
+  return (
+    <Box>
+      <chakra.h1 color='tomato'>Hello World!</chakra.h1>
+    </Box>
+  );
+};
+
+export default Home;
